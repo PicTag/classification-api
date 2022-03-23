d4046e96729e55fa7423cf8bff1db9284d8fff3e
YuanSamuel <samuel6161@gmail.com>
2022-03-23 01:51:18 -0500

    :sparkles: update

--- application/server/main.py
+++ application/server/main.py
@@ -3,12 +3,8 @@ from fastapi import FastAPI, File, UploadFile
 from starlette.responses import RedirectResponse
 
 from application.components import predict, read_imagefile
-from application.schema import Symptom
-from application.components.prediction import symptom_check
 
-app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
-<h2>Try Covid symptom checker api - it is just a learning app demo</h2>
-<br>by Aniket Maurya"""
+app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>"""
 
 app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
 
@@ -20,19 +16,15 @@ async def index():
 
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    print('hey there');
+    extension = file.filename.split(".")[-1].lower() in ("jpg", "jpeg", "png")
     if not extension:
         return "Image must be jpg or png format!"
     image = read_imagefile(await file.read())
+    print('here');
     prediction = predict(image)
 
     return prediction
-
-
-@app.post("/api/covid-symptom-check")
-def check_risk(symptom: Symptom):
-    return symptom_check.get_risk_level(symptom)
-
 
 if __name__ == "__main__":
     uvicorn.run(app, debug=True)
